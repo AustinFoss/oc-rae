4d41d6b169ccb3a66432680debc348d1fba01b22
Austin Foss <austin.foss.account@protonmail.com>
2021-01-06 09:07:18 -0700

    Update piCam.py

--- scripts/piCam.py
+++ scripts/piCam.py
@@ -5,7 +5,7 @@ camera = PiCamera()
 
 def snapPhoto(name):
 
-    camera.resolution = (1920, 1080)
+    camera.resolution = (3280, 2464)
     camera.start_preview()
     sleep(2) # Camera warm-up time
     camera.capture('/home/pi/oc-rae/Pictures/' + name + '.jpg')
